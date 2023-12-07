43fe94d81718deceea6fda7d061d2bd1883c3420
LiShuHao <2423779636@qq.com>
2023-12-07 22:24:01 +0800

    111

--- reptile_IEEE.py
+++ reptile_IEEE.py
@@ -11,28 +11,46 @@ def spider(start_url):
     # 导出搜索的全部记录选项
     browser.get(start_url)
     browser.execute_script("window.location.href+='/keywords#keywords'")
+    try:
+        browser.find_elements_by_class_name('stats-keywords-container')
+    except:
+        return "null"
     # 获取keyword的容器位置
     container = browser.find_elements_by_class_name('stats-keywords-container')
+
+    try:
+        container[1].find_elements_by_class_name('doc-keywords-list-item')
+    except:
+        return "null"
     # 获取keywords列表
     list_item = container[1].find_elements_by_class_name('doc-keywords-list-item')
+
+    try:
+        list_item[0].text.lstrip('IEEE Keywords\n').replace('\n,\n', '[]')
+    except:
+        return "null"
     # 获取具体的keywords值列表
     text = list_item[0].text.lstrip('IEEE Keywords\n').replace('\n,\n', '[]')
-    print(text)
     return text
 
 
 def ieee_spider():
     content = []
-    with open('IEEE_journal.json', 'r') as file:
-        content = file.read()
-        content = json.loads(content)
-        for data in content:
-            keywords = spider(data['url'])
-            data['keywords'] = keywords
-            print(data)
-            time.sleep(3)
-    with open('IEEE_journal1.json', 'w') as file:
-        json.dump(content, file, indent=4)
+    for index in range(100):
+        try:
+            with open(f'documents/batch_{index + 1}.json', 'r') as file:
+                content = file.read()
+                content = json.loads(content)
+                for data in content:
+                    keywords = spider(data['url'])
+                    data['keywords'] = keywords
+                    print(data)
+                    time.sleep(3)
+            with open(f'IEEEData/IEEE_journal{index + 1}.json', 'w') as file:
+                json.dump(content, file, indent=4)
+        except:
+            with open('error.txt', 'w') as file:
+                file.write(str(index+1))
 
 
 if __name__ == "__main__":
